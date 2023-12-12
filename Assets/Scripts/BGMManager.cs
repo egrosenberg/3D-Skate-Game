c6747575decc9a8c3fe3fff9bcba56da016e3a7b
Ethan Rosenberg <erosenberg2019@hotmail.com>
2023-12-12 15:50:10 -0800

    Music now plays from an array, swapped BGM

--- Assets/Scripts/BGMManager.cs
+++ Assets/Scripts/BGMManager.cs
@@ -3,16 +3,14 @@ using UnityEngine.UI;
 
 public class BGMManager : MonoBehaviour
 {
-    public AudioClip bgm1;
-    public AudioClip bgm2;
+    public AudioClip[] m_BackgroundMusic;
 
     public Slider volumeSlider; // Reference to the volume slider in the UI
 
-    // Set manual default volumes for each AudioClip
-    public float defaultVolumeBGM1 = 0.2f;
-    public float defaultVolumeBGM2 = 0.1f;
+    public float m_DefaultVolume = 0.2f;
 
     private AudioSource audioSource;
+    private int m_TrackNumber;
 
     private void Start()
     {
@@ -21,12 +19,11 @@ public class BGMManager : MonoBehaviour
         audioSource.loop = false; // We'll handle looping manually
 
         // Set default volumes for each AudioClip
-        audioSource.volume = (audioSource.clip == bgm1) ? defaultVolumeBGM1 : defaultVolumeBGM2;
+        audioSource.volume = m_DefaultVolume;
 
-        // Set up the volume slider callback
-        // volumeSlider.onValueChanged.AddListener(ChangeVolume);
+        m_TrackNumber = 0;
 
-        PlayBGM(bgm1); // Start playing the first BGM
+        PlayBGM(m_BackgroundMusic[m_TrackNumber]); // Start playing the first BGM
     }
 
     private void Update()
@@ -34,19 +31,14 @@ public class BGMManager : MonoBehaviour
         // Check if the current BGM has finished playing
         if (!audioSource.isPlaying)
         {
-            // Switch to the other BGM and play it
-            if (audioSource.clip == bgm1)
+            //Play next in queue
+            m_TrackNumber++;
+            if (m_TrackNumber >= m_BackgroundMusic.Length)
             {
-                PlayBGM(bgm2);
+                m_TrackNumber = 0;
             }
-            else
-            {
-                PlayBGM(bgm1);
-            }
+            PlayBGM(m_BackgroundMusic[m_TrackNumber]);
         }
-
-        // // Manual Controls
-        // HandleManualControls();
     }
 
     private void PlayBGM(AudioClip clip)
@@ -54,28 +46,6 @@ public class BGMManager : MonoBehaviour
         // Set the new BGM clip and start playing
         audioSource.clip = clip;
         audioSource.Play();
-    }
-
-    // private void HandleManualControls()
-    // {
-    //     // Toggle between BGMs when pressing the 'Space' key
-    //     if (Input.GetKeyDown(KeyCode.Space))
-    //     {
-    //         ToggleBGM();
-    //     }
-    // }
-
-    private void ToggleBGM()
-    {
-        // Toggle between the two BGMs
-        if (audioSource.clip == bgm1)
-        {
-            PlayBGM(bgm2);
-        }
-        else
-        {
-            PlayBGM(bgm1);
-        }
     }
 
     public void ChangeVolume(float newVolume)
@@ -92,19 +62,7 @@ public class BGMManager : MonoBehaviour
         }
         else
         {
-            audioSource.volume = defaultVolumeBGM1;
+            audioSource.volume = m_DefaultVolume;
         }
     }
-
-    // private void IncreaseVolume()
-    // {
-    //     // Increase the volume (clamp between 0 and 1)
-    //     audioSource.volume = Mathf.Clamp01(audioSource.volume + 0.05f);
-    // }
-
-    // private void DecreaseVolume()
-    // {
-    //     // Decrease the volume (clamp between 0 and 1)
-    //     audioSource.volume = Mathf.Clamp01(audioSource.volume - 0.05f);
-    // }
 }
